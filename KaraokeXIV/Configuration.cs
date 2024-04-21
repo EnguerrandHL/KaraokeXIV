c5cf6cc11e1b6844a3ee94f8325220ea85ba7b31
EnguerrandHL <enguerrand.hollier-larousse@efrei.net>
2024-04-21 13:20:43 +0200

    Change project name

--- KaraokeXIV/Configuration.cs
+++ KaraokeXIV/Configuration.cs
@@ -2,7 +2,7 @@
 using Dalamud.Plugin;
 using System;
 
-namespace SamplePlugin;
+namespace KaraokeXIV;
 
 [Serializable]
 public class Configuration : IPluginConfiguration
--- KaraokeXIV/Plugin.cs
+++ KaraokeXIV/Plugin.cs
@@ -4,9 +4,9 @@ using Dalamud.Plugin;
 using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
-using SamplePlugin.Windows;
+using KaraokeXIV.Windows;
 
-namespace SamplePlugin;
+namespace KaraokeXIV;
 
 public sealed class Plugin : IDalamudPlugin
 {
@@ -16,7 +16,7 @@ public sealed class Plugin : IDalamudPlugin
     private ICommandManager CommandManager { get; init; }
     public Configuration Configuration { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
+    public readonly WindowSystem WindowSystem = new("KaraokeXIV");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
 
